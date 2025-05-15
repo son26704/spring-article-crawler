88c4ca84c6a8540de8f4b093197ac745fdbb6bc7
Nguyen Trung Son <nguyentrungson267@gmail.com>
2025-05-15 10:21:44 +0700

    Implement Redis Cluster

--- src/main/java/com/dantri/crawler/config/RedisConfig.java
+++ src/main/java/com/dantri/crawler/config/RedisConfig.java
@@ -2,6 +2,7 @@ package com.dantri.crawler.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
@@ -10,7 +11,14 @@ public class RedisConfig {
 
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        RedisClusterConfiguration clusterConfig = new RedisClusterConfiguration();
+        clusterConfig.clusterNode("localhost", 6379);
+        clusterConfig.clusterNode("localhost", 6380);
+        clusterConfig.clusterNode("localhost", 6381);
+        clusterConfig.clusterNode("localhost", 6382);
+        clusterConfig.clusterNode("localhost", 6383);
+        clusterConfig.clusterNode("localhost", 6384);
+        return new LettuceConnectionFactory(clusterConfig);
     }
 
     @Bean
